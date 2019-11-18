5cb691341435bee2751c16bea65b3122af102f95
Ove Bastiansen <ovebastiansen@outlook.com>
2019-11-18 22:07:47 +0100

    created a simple repro of issue

---
+++ TestFixtureSourceError/TestFactory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace TestFixtureSourceError
+{
+    public static class Settings
+    {
+        public static bool SingleBrowser { get; set; } = false;
+    }
+
+    public class TestCaseDefinition
+    {
+        public string Os { get; set; }
+        public string Browser { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Os} {Browser}";
+        }
+    }
+
+    public static class TestFactory
+    {
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                var cases = GetTests(Settings.SingleBrowser);
+                return cases;
+            }
+        }
+
+        private static IEnumerable GetTests(bool singleBrowser)
+        {
+            var cases = new List<TestCaseDefinition>();
+            cases.Add(new TestCaseDefinition { Os = "Windows", Browser = "Google" });
+            if(!singleBrowser)
+            {
+                cases.Add(new TestCaseDefinition { Os = "Windows", Browser = "Edgium" });
+            }
+            return cases;
+        }
+    }
+}
